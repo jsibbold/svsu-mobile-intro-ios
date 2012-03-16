b717e2a35cf6c2f12542a2be820e5cfd971a6344
Brian Munzenberger <brian@detroitlabs.com>
2012-03-16 15:21:38 -0400

    Initial app creation, add navigation controller

---
+++ step1/SVSUIntro/SVSUIntro/SVSUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVSUAppDelegate.h
+//  SVSUIntro
+//
+//  Created by Brian Munzenberger on 3/16/12.
+//  Copyright (c) 2012 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVSUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ step1/SVSUIntro/SVSUIntro/SVSUViewController.m
@@ -0,0 +1,34 @@
+//
+//  SVSUViewController.m
+//  SVSUIntro
+//
+//  Created by Brian Munzenberger on 3/16/12.
+//  Copyright (c) 2012 Detroit Labs. All rights reserved.
+//
+
+#import "SVSUViewController.h"
+
+@interface SVSUViewController ()
+
+@end
+
+@implementation SVSUViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
